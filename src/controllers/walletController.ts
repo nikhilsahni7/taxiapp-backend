d08f3876e7369911f1a16ad22265885e733f9511
Nikhil Sahni <nikhil.sahni321@gmail.com>
2025-06-14 23:49:27 +0530

    fix: update Razorpay key_secret to be non-nullable in walletController

--- src/controllers/walletController.ts
+++ src/controllers/walletController.ts
@@ -9,7 +9,7 @@ import Razorpay from "razorpay";
 const prisma = new PrismaClient();
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID!,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
+  key_secret: process.env.RAZORPAY_SECRET!,
 });
 
 // Driver initiates wallet top-up
